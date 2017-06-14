5e113cefc65d5a49311ccf47b81af70a990eb715
Narin Luangrath <narin.luangrath@gmail.com>
2017-06-14 03:58:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,24 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @p = params
+    erb :team
+  end
+
+
 
 end
+
+# The form should have fields for:
+# Team name ('name')
+# Coach ('coach')
+# Point Guard ('pg')
+# Shooting Guard ('sg')
+# Power Forward ('pf')
+# Small Forward ('sf')
+# Center ('c')
